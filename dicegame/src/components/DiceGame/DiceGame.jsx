0a3681c5b7031842b9cf7c5d153f7051d20b4a45
Emanuel Yaish <emanuelyaish@gmail.com>
2021-12-16 11:48:50 +0200

    start files

---
+++ dicegame/src/components/DiceGame/DiceGame.jsx
@@ -0,0 +1,12 @@
+import Player from "../Player/Player";
+import "./DiceGame.css";
+
+function DiceGame() {
+  return (
+    <div className="App">
+      <Player PlayerName="1" />
+    </div>
+  );
+}
+
+export default DiceGame;
